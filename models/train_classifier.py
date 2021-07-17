524c3da72abdb041db2b13dd43befec29a476880
Kaique Colognesi <kaique.c.oliveira@hotmail.com>
2021-07-17 15:49:15 -0300

    Adding first files to repository

---
+++ models/train_classifier.py
@@ -0,0 +1,97 @@
+import sys
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+import pickle
+
+
+def load_data(database_filepath):
+    engine = create_engine(database_filepath)
+    df = pd.read_sql_table('disaster', engine)
+    X = df['message']
+    Y = df.iloc[: ,3:]
+    return X, Y
+
+def tokenize(text):
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+    return clean_tokens
+
+
+def build_model():
+    pipeline = Pipeline([
+                ('vect', CountVectorizer(tokenizer = tokenize)),
+                ('tfidf', TfidfTransformer()),
+                ('clf', MultiOutputClassifier(RandomForestClassifier()))
+            ])
+
+    # I'm not using more parameters for it could take a long time to finish the fit.
+    parameters = {
+    'clf__estimator__criterion':['gini', 'entropy'],
+    'clf__estimator__n_estimators':[50,100]
+    }
+
+    cv = GridSearchCV(pipeline, param_grid = parameters, n_jobs = -1, verbose = 2, cv = 2)
+
+    return cv
+
+def evaluate_model(model, X_test, Y_test):
+    categories = Y_test.columns.tolist()
+    y_pred = model.predict(X_test)
+    y_pred = pd.DataFrame(y_pred, columns=categories)
+
+    for i, var in enumerate(Y_test.columns):
+        print(var)
+        print(classification_report(Y_test.iloc[:, i], y_pred.iloc[:, i], zero_division = 0))
+
+
+def save_model(model, model_filepath):
+    with open(model_filepath, 'wb') as picklepath:
+        pickle.dump(model, picklepath)
+        picklepath.close()
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
